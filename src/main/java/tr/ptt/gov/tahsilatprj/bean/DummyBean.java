522e244162e1699f92247d9dc4ffb4e372f7c4ef
BEM <BEM@10.0.0.214>
2015-08-31 15:45:28 +0300

    ilk ekleme

---
+++ src/main/java/tr/ptt/gov/tahsilatprj/bean/DummyBean.java
@@ -0,0 +1,33 @@
+
+package tr.ptt.gov.tahsilatprj.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class DummyBean {
+    
+    private String ad;
+    private String sifre;
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSifre() {
+        return sifre;
+    }
+
+    public void setSifre(String sifre) {
+        this.sifre = sifre;
+    }
+    public String sayfayaGit()
+    {
+      return "dummy.xhtml";
+    }
+}
